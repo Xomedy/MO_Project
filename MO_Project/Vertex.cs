5b5653773807ced7714b6c6a20d84711c8631a23
Xomedy <lyutikenot@gmail.com>
2019-09-19 10:42:09 +0300

    Добавьте файлы проекта.

---
+++ MO_Project/Vertex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace MO_Project
+{
+    class Vertex
+    {
+        private double posX;
+        private double posY;
+        private List<Vertex> parentVertices;
+        private List<Vertex> inheritorVertices;
+    }
+}
