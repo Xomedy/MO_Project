337ee7bf44c1add7b33b6027349b7d204f57fb3e
Xomedy <lyutikenot@gmail.com>
2019-09-20 11:29:51 +0300

    Доделал все конструкторы, добавил свойства

--- MO_Project/Program.cs
+++ MO_Project/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static System.Console;
+using System.Collections.Generic;
 
 namespace MO_Project
 {
@@ -7,7 +8,11 @@ namespace MO_Project
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            Vertex vrt1 = new Vertex(0.9, 0.3, "", 1);
+            List<Vertex> lvp = new List<Vertex> { new Vertex(13, 13), new Vertex(81, 81) };
+            List<Vertex> lvc = new List<Vertex> { new Vertex(19, 19), new Vertex(89, 89) };
+            Vertex vrt2 = new Vertex(1, 1, "V1", null ,lvc ,lvp);
+            WriteLine(vrt2);
         }
     }
 }
--- MO_Project/Vertex.cs
+++ MO_Project/Vertex.cs
@@ -10,9 +10,9 @@ namespace MO_Project
         private double posX;
         private double posY;
         private List<Vertex> parentVertices;
-        private List<Vertex> inheritorVertices;
+        private List<Vertex> childVertices;
         private string name;
-        private int value;
+        private int? weight;
         /// <summary>
         /// Конструктор без параметров, который создает вершину с координатами (0;0),
         /// стандартным именем и весом 1.
@@ -22,8 +22,9 @@ namespace MO_Project
             posX = 0;
             posY = 0;
             name = "vertex";
-            value = 1;
+            weight = 1;
         }
+
         /// <summary>
         /// Конструктор, который создаёт вершину с координатами (x;y),
         /// стандартным именем и весом 1
@@ -35,7 +36,73 @@ namespace MO_Project
             posX = x;
             posY = y;
             name = "vertex";
-            value = 1;
+            weight = 1;
         }
+
+        /// <summary>
+        /// Конструктор, который создаёт вершину с координатами (х;у),
+        /// заданным именем и весом
+        /// </summary>
+        /// <param name="x">Координата по оси Х</param>
+        /// <param name="y">Координата по оси У</param>
+        /// <param name="name">Имя вершины</param>
+        /// <param name="weight">Вес вершины</param>
+        public Vertex (double x, double y, string name, int? weight)
+        {
+            posX = x;
+            posY = y;
+            this.name = name;
+            this.weight = weight;
+        }
+
+        /// <summary>
+        /// Конструктор, который создаёт вершину с координатами (х;у),
+        /// заданным именем, весом, списком вершин потомков и родителей
+        /// </summary>
+        /// <param name="x">Координата по оси Х</param>
+        /// <param name="y">Координата по оси У</param>
+        /// <param name="name">Имя вершины</param>
+        /// <param name="weight">Вес вершины</param>
+        /// <param name="childs">Список вершин-потомков данной вершины.</param>
+        /// <param name="parents">Список вершин-родителей данной вершины.</param>
+        public Vertex (double x, double y, string name, int? weight, List<Vertex> childs, List<Vertex> parents)
+        {
+            posX = x;
+            posY = y;
+            this.name = name;
+            this.weight = weight;
+            parentVertices = parents;
+            childVertices = childs;
+        }
+
+        /// <summary>
+        /// Координата Х
+        /// </summary>
+        public double PositionX { get { return posX; } set { posX = value; } }
+
+        /// <summary>
+        /// Координата У
+        /// </summary>
+        public double PositionY { get { return posY; } set { posY = value; } }
+
+        /// <summary>
+        /// Имя вершины
+        /// </summary>
+        public string Name { get { return name; } set { name = value; } }
+
+        /// <summary>
+        /// Вес вершины
+        /// </summary>
+        public int? Weight { get { return weight; } set { weight = value; } }
+
+        /// <summary>
+        /// Получить список вершин-родителей данной вершины
+        /// </summary>
+        public List<Vertex> GetParents { get { return parentVertices; } }
+
+        /// <summary>
+        /// Получить список вершин-потомков данной вершины
+        /// </summary>
+        public List<Vertex> GetChilds { get { return childVertices; } }
     }
 }
